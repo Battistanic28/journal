f527bd8abf2c878bd4e62a5e11dd311c566eb432
Nick <n.battista180@gmail.com>
2026-06-12 19:24:50 -0600

    wip

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,8 @@ const ramblings = defineCollection({
   loader: glob({ pattern: "*.mdx", base: "./src/content/ramblings" }),
   schema: z.object({
     title: z.string(),
-    published: z.string(),
+    published: z.string(), // TODO: use date type - rename to pubDate
+    slug: z.string()
   }),
 });
 
