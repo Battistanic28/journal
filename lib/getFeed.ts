e8cc630fa26b02b170fa0308bb8d3c309ea256f4
Nick <n.battista180@gmail.com>
2026-06-19 16:49:51 -0600

    feed - wip

--- lib/getFeed.ts
+++ lib/getFeed.ts
@@ -8,23 +8,28 @@ If pubdate is greater than 3 days old, do not fetch/display
 export const getFeed = async () => {
   const sources = Object.keys(feed);
 
-  const feedData = await Promise.all(sources.map((source) => {
-    return Promise.all(feed[source].map(async (url) => {
-      const res = await fetch(url);
-      const data = await res.text();
-      const parser = new XMLParser();
-      const xmlDoc = parser.parse(data);
+  const feedData = await Promise.all(
+    sources.map((source) => {
+      return Promise.all(
+        feed[source].map(async (url) => {
+          const res = await fetch(url);
+          const data = await res.text();
+          const parser = new XMLParser();
+          const xmlDoc = parser.parse(data);
 
-      const title = xmlDoc.rss.channel.item[0].title;
-      const desc = xmlDoc.rss.channel.item[0].description;
-      const link = xmlDoc.rss.channel.item[0].link;
-      const pubDate = xmlDoc.rss.channel.item[0].pubDate;
+          const channel = xmlDoc.rss.channel.title;
+          const title = xmlDoc.rss.channel.item[0].title;
+          const desc = xmlDoc.rss.channel.item[0].description;
+          const link = xmlDoc.rss.channel.item[0].link;
+          const pubDate = xmlDoc.rss.channel.item[0].pubDate;
 
-      return { title, desc, link, pubDate };
-    }));
-  }));
+          return { channel, title, desc, link, pubDate };
+        }),
+      );
+    }),
+  );
 
-//   const feedData = await Promise.all(urls.map(async (url) => {}));
+  //   const feedData = await Promise.all(urls.map(async (url) => {}));
 
   return feedData;
 };
