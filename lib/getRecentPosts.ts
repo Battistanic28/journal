84dbec45ee73c5300f7cc65f8a0fa18e4b32cff0
Nick <n.battista180@gmail.com>
2026-07-02 13:34:37 -0600

    add ramblings page for all posts

--- lib/getRecentPosts.ts
+++ lib/getRecentPosts.ts
@@ -1,10 +1,14 @@
 import { getCollection } from "astro:content"
 
 // returns array of 5 most recent posts in reverse chronological order
-export const getRecentPosts = async () => {
+export const getRecentPosts = async (count?: number) => {
     const posts = await getCollection('ramblings')
 
     const sortedPosts = posts.sort((a, b) => new Date(b.data.published) - new Date(a.data.published));
 
-    return sortedPosts.slice(0,5)
+    if (count) {
+        return sortedPosts.slice(0,count)
+    }
+
+    return sortedPosts
 }
