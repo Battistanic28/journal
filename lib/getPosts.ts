5224c020dd65f8693278297dc883e6b6ad1db023
Nick <n.battista180@gmail.com>
2026-07-02 18:38:30 -0600

    refactor feed

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content"
 
 // returns array of 5 most recent posts in reverse chronological order
-export const getRecentPosts = async (count?: number) => {
+export const getPosts = async (count?: number) => {
     const posts = await getCollection('ramblings')
 
     const sortedPosts = posts.sort((a, b) => new Date(b.data.published) - new Date(a.data.published));
